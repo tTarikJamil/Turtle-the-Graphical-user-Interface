df7ecbfc6d159919b289207fa176ff7ca56c9eab
TARIK JAMIL <162028394+tTarikJamil@users.noreply.github.com>
2025-08-14 11:38:57 +0600

    main

---
+++ Random Walk.py
@@ -0,0 +1,37 @@
+########### Random Walk ########
+import random
+from turtle import Turtle,Screen
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+tinny = Turtle()
+Direction = [0,90,180,270]
+tinny.pensize(10)
+tinny.speed('fast')
+for _ in range(200):
+    tinny.color(random.choice(colours))
+    tinny.forward(30)
+    tinny.setheading(random.choice(Direction))
+screen = Screen()
+screen.exitonclick()
+
+########### Random Walk With RGB Colours ########
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+directions = [0, 90, 180, 270]
+tim.pensize(10)
+tim.speed("fastest")
+
+for _ in range (200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
